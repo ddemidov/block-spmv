0b154826d187b69dec228b388e1ef278c13633bf
Denis Demidov <dennis.demidov@gmail.com>
2016-10-17 16:00:20 +0300

    Assewmble block-valued matrix

--- bench.cpp
+++ bench.cpp
@@ -4,6 +4,7 @@
 #include <boost/program_options.hpp>
 #include <amgcl/value_type/static_matrix.hpp>
 #include <amgcl/adapter/block_matrix.hpp>
+#include <amgcl/backend/vexcl_static_matrix.hpp>
 
 #include <amgcl/io/mm.hpp>
 #include <amgcl/profiler.hpp>
@@ -11,8 +12,20 @@
 #include <vexcl/vexcl.hpp>
 #include <vexcl/sparse/ell.hpp>
 
+template <int N, int M>
+using block = amgcl::static_matrix<double, N, M>;
+
 //---------------------------------------------------------------------------
-int poisson3d(int n, std::vector<int> &ptr, std::vector<int> &col, std::vector<double> &val) {
+template <int B>
+int poisson3d(
+        int n,
+        std::vector<int> &ptr,
+        std::vector<int> &col,
+        std::vector< block<B,B> > &val
+        )
+{
+    namespace math = amgcl::math;
+
     int n3  = n * n * n;
 
     ptr.clear();
@@ -23,41 +36,42 @@ int poisson3d(int n, std::vector<int> &ptr, std::vector<int> &col, std::vector<d
     col.reserve(n3 * 7);
     val.reserve(n3 * 7);
 
+    block<B,B> one = math::constant<block<B,B>>(1.0);
     ptr.push_back(0);
     for(int k = 0, idx = 0; k < n; ++k) {
         for(int j = 0; j < n; ++j) {
             for (int i = 0; i < n; ++i, ++idx) {
                 if (k > 0) {
                     col.push_back(idx - n * n);
-                    val.push_back(-0.25);
+                    val.push_back(-0.25 * one);
                 }
 
                 if (j > 0) {
                     col.push_back(idx - n);
-                    val.push_back(-0.25);
+                    val.push_back(-0.25 * one);
                 }
 
                 if (i > 0) {
                     col.push_back(idx - 1);
-                    val.push_back(-0.25);
+                    val.push_back(-0.25 * one);
                 }
 
                 col.push_back(idx);
-                val.push_back(1.0);
+                val.push_back(1.0 * one);
 
                 if (i + 1 < n) {
                     col.push_back(idx + 1);
-                    val.push_back(-0.25);
+                    val.push_back(-0.25 * one);
                 }
 
                 if (j + 1 < n) {
                     col.push_back(idx + n);
-                    val.push_back(-0.25);
+                    val.push_back(-0.25 * one);
                 }
 
                 if (k + 1 < n) {
                     col.push_back(idx + n * n);
-                    val.push_back(-0.25);
+                    val.push_back(-0.25 * one);
                 }
 
                 ptr.push_back( col.size() );
@@ -66,6 +80,38 @@ int poisson3d(int n, std::vector<int> &ptr, std::vector<int> &col, std::vector<d
     }
 
     return n3;
+}
+
+//---------------------------------------------------------------------------
+template <int B>
+void run_benchmark(int m) {
+    vex::Context ctx(vex::Filter::Env && vex::Filter::Count(1));
+    std::cout << ctx << std::endl;
+
+    amgcl::backend::enable_static_matrix_for_vexcl(ctx);
+
+    vex::profiler<> prof(ctx);
+    prof.tic_cpu("assemble");
+    std::vector<int> ptr, col;
+    std::vector< block<B,B> > val;
+    int n = poisson3d(m, ptr, col, val);
+    prof.toc("assemble");
+
+    prof.tic_cl("transfer");
+    vex::sparse::ell<block<B,B>,int> A(ctx, n, n, ptr, col, val);
+    prof.toc("transfer");
+
+    vex::vector<block<B,1>> x(ctx, n), y(ctx, n);
+
+    x = amgcl::math::constant<block<B,1>>(1.0);
+    y = A * x;
+
+    prof.tic_cl("spmv (scalar) x100");
+    for(int i = 0; i < 100; ++i)
+        y = A * x;
+    prof.toc("spmv (scalar) x100");
+
+    std::cout << prof << std::endl;
 }
 
 //---------------------------------------------------------------------------
@@ -79,7 +125,7 @@ int main(int argc, char *argv[]) {
         ("help,h", "Show this help.")
         (
          "block-size,b",
-         po::value<int>()->default_value(1),
+         po::value<int>()->default_value(4),
          "The block size of the system matrix. "
          "When specified, the system matrix is assumed to have block-wise structure. "
          "This usually is the case for problems in elasticity, structural mechanics, "
@@ -104,29 +150,12 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    vex::Context ctx(vex::Filter::Env && vex::Filter::Count(1));
-    std::cout << ctx << std::endl;
-
-    vex::profiler<> prof(ctx);
-    prof.tic_cpu("assemble");
-    std::vector<int> ptr, col;
-    std::vector<double> val;
-    int n = poisson3d(vm["size"].as<int>(), ptr, col, val);
-    prof.toc("assemble");
-
-    prof.tic_cl("transfer");
-    vex::sparse::ell<double,int> A(ctx, n, n, ptr, col, val);
-    prof.toc("transfer");
-
-    vex::vector<double> x(ctx, n), y(ctx, n);
-
-    x = 1;
-    y = A * x;
-
-    prof.tic_cl("spmv (scalar) x100");
-    for(int i = 0; i < 100; ++i)
-        y = A * x;
-    prof.toc("spmv (scalar) x100");
-
-    std::cout << prof << std::endl;
+    switch(vm["block-size"].as<int>()) {
+        case 4:
+            run_benchmark<4>(vm["size"].as<int>());
+            break;
+        default:
+            std::cerr << "Unsopported block size" << std::endl;
+            return 1;
+    }
 }
